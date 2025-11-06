a79c9a8c4cf5acc0967a727a8e81692d01642b09
mhtvsSFrpHdE <2bemperor47@gmail.com>
2025-11-07 05:18:55 +0800

    Patch AgmgPinned110725

--- Patch.cpp
+++ Patch.cpp
@@ -4,6 +4,143 @@
 #include <vector>
 #include <wx/file.h>
 
+enum class IniFaceVersion {
+    Unknown,
+    Pre6_1,    // Before version 6.1
+    OutdatedNews102425,    // Gamebanana news how to fix face, but outdated, will not work
+    AgmgPinned110725    // AGMG discord pinned message how to fix face, passed test
+};
+
+IniFaceVersion GuessIniFaceVersion(std::vector<wxString>& lines, size_t faceConfigGroupBeginLineNumber) {
+    bool pre6_1 = false;
+    bool outdatedNews102425 = false;
+    bool agmgPinned110725 = false;
+    for (size_t i = faceConfigGroupBeginLineNumber; i < lines.size(); i++) {
+        auto line = lines[i];
+        auto lowerLine = line.Lower();
+
+        if (lowerLine.Contains("ps-t0")) {
+            pre6_1 = true;
+        }
+        if (lowerLine.Contains("this") && lowerLine.Contains("facehead")) {
+            outdatedNews102425 = true;
+        }
+        if (lowerLine.Contains("resource\\gimi\\")) {
+            agmgPinned110725 = true;
+        }
+
+        bool endFaceConfigGroup = lowerLine.StartsWith("[");
+        if (endFaceConfigGroup) {
+            break;
+        }
+    }
+
+    IniFaceVersion result = IniFaceVersion::Unknown;
+    if (outdatedNews102425) {
+        result = IniFaceVersion::OutdatedNews102425;
+    }
+    else if (pre6_1) {
+        result = IniFaceVersion::Pre6_1;
+    }
+    else if (agmgPinned110725) {
+        result = IniFaceVersion::AgmgPinned110725;
+    }
+    return result;
+}
+
+void PatchPre6_1(std::vector<wxString>& lines, size_t faceConfigGroupBeginLineNumber) {
+    std::cout << "Ini file version seems is Pre6.1, processing" << std::endl;
+
+    // Patch exist line
+    for (size_t i = faceConfigGroupBeginLineNumber; i < lines.size(); i++)
+    {
+        auto line = lines[i];
+        auto lowerLine = line.Lower();
+
+        // Diffuse map
+        if (lowerLine.Contains("ps-t0")) {
+            auto splittedString = wxSplit(line, '=');
+            wxString diffuseMapName = splittedString.Last().Trim().Trim(false);
+
+            auto textBeginIndex = lowerLine.Find("ps-t0");
+            auto newLineWithIndent = line.substr(0, textBeginIndex);
+            newLineWithIndent = newLineWithIndent + "Resource\\GIMI\\Diffuse = ref " + diffuseMapName;
+            lines[i] = newLineWithIndent;
+            std::cout << "Line " << i + 1 << " changed to: " << line << std::endl;
+        }
+
+        // Light map
+        if (lowerLine.Contains("ps-t1")) {
+            auto splittedString = wxSplit(line, '=');
+            wxString lightMapName = splittedString.Last().Trim().Trim(false);
+
+            auto textBeginIndex = lowerLine.Find("ps-t1");
+            auto newLineWithIndent = line.substr(0, textBeginIndex);
+            newLineWithIndent = newLineWithIndent + "Resource\\GIMI\\LightMap = ref " + lightMapName;
+            lines[i] = newLineWithIndent;
+            std::cout << "Line " << i + 1 << " changed to: " << line << std::endl;
+        }
+
+        bool endFaceConfigGroup = lowerLine.StartsWith("[");
+        if (endFaceConfigGroup) {
+            break;
+        }
+
+    }
+
+    // Insert new line
+    bool isResourceSection = false;
+    wxString newLineWithIndent = wxEmptyString;
+    for (size_t i = faceConfigGroupBeginLineNumber; i < lines.size(); i++)
+    {
+        auto line = lines[i];
+        auto lowerLine = line.Lower();
+        if (isResourceSection) {
+            bool endResourceSection = lowerLine.Contains("resource\\gimi\\") == false;
+            if (endResourceSection) {
+                isResourceSection = false;
+
+                auto newLine = newLineWithIndent + "run = CommandList\\GIMI\\SetTextures";
+                lines.insert(lines.begin() + i, newLine);
+                std::cout << "New line at " << i + 1 << ": " << newLine << std::endl;
+            }
+        }
+        else {
+            isResourceSection = lowerLine.Contains("resource\\gimi\\");
+            auto textBeginIndex = lowerLine.Find("resource\\gimi\\");
+            newLineWithIndent = line.substr(0, textBeginIndex);
+        }
+    }
+}
+
+void UndoOutdatedNews102425ToPre6_1(std::vector<wxString>& lines, size_t faceConfigGroupBeginLineNumber) {
+    std::cout << "Ini file version seems is OutdatedNews102425, processing" << std::endl;
+
+    bool isFaceConfigGroup = false;
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+        auto line = lines[i];
+        auto lowerLine = line.Lower();
+        if (isFaceConfigGroup) {
+            bool isPsT0 = lowerLine.Contains("this");
+            if (isPsT0) {
+                line.Replace("this", "ps-t0");
+                lines[i] = line;
+                std::cout << "Line " << i + 1 << " changed to: " << line << std::endl;
+            }
+            bool endFaceConfigGroup = lowerLine.StartsWith("[");
+            if (endFaceConfigGroup) {
+                isFaceConfigGroup = false;
+            }
+        }
+        else {
+            isFaceConfigGroup = lowerLine.StartsWith("[textureoverride") && lowerLine.EndsWith("faceheaddiffuse]");
+        }
+    }
+
+    std::cout << "Downgraded ini file version back to Pre6_1" << std::endl;
+}
+
 ReturnValue<> Patch::PatchORFix_6_1(wxString iniFileName) {
     ReturnValue<> result;
     wxFileName basePath(iniFileName);
@@ -20,17 +157,35 @@ ReturnValue<> Patch::PatchORFix_6_1(wxString iniFileName) {
     }
 
     bool isFaceConfigGroup = false;
+    bool groupIsPatched = false;
     for (size_t i = 0; i < lines.size(); i++)
     {
         auto line = lines[i];
         auto lowerLine = line.Lower();
-        if (isFaceConfigGroup) {
-            bool isPsT0 = lowerLine.StartsWith("ps-t0");
-            if (isPsT0) {
-                line.Replace("ps-t0", "this");
-                lines[i] = line;
-                std::cout << "Line " << i + 1 << " changed to: " << line << std::endl;
+        if (isFaceConfigGroup && groupIsPatched == false) {
+            auto iniFaceVersion = GuessIniFaceVersion(lines, i);
+            if (iniFaceVersion == IniFaceVersion::Pre6_1) {
+                PatchPre6_1(lines, i);
+                groupIsPatched = true;
             }
+            if (iniFaceVersion == IniFaceVersion::OutdatedNews102425) {
+                UndoOutdatedNews102425ToPre6_1(lines, i);
+                PatchPre6_1(lines, i);
+                groupIsPatched = true;
+            }
+            if (iniFaceVersion == IniFaceVersion::AgmgPinned110725) {
+                std::cout << "Ini file version seems is AgmgPinned110725, nothing to do" << std::endl;
+            }
+            if (iniFaceVersion == IniFaceVersion::Unknown) {
+                std::cout << "Your ini status is unknown, please fix it manually" << std::endl
+                    << "or maybe it doesn't need to be fixed" << std::endl
+                    << "Face fix tutorial: https://github.com/mhtvsSFrpHdE/ORFixFaceApplierCpp/wiki/How-to-fix-faces" << std::endl
+                    << "Press Enter to continue process other ini file";
+                std::string ignore;
+                std::getline(std::cin, ignore);
+                return result;
+            }
+
             bool endFaceConfigGroup = lowerLine.StartsWith("[");
             if (endFaceConfigGroup) {
                 isFaceConfigGroup = false;
@@ -38,6 +193,7 @@ ReturnValue<> Patch::PatchORFix_6_1(wxString iniFileName) {
         }
         else {
             isFaceConfigGroup = lowerLine.StartsWith("[textureoverride") && lowerLine.EndsWith("faceheaddiffuse]");
+            groupIsPatched = false;
             if (isFaceConfigGroup) { std::cout << line << std::endl; }
         }
     }
